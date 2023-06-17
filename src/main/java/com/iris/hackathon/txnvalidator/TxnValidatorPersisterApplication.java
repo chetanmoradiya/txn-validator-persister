9271f8a551649bbe9b11811b2a7de9095f96f18f
chetanmoradiya <chetan.moradiya@gmail.com>
2023-06-17 19:15:59 +0530

    first commit

---
+++ src/main/java/com/iris/hackathon/txnvalidator/TxnValidatorPersisterApplication.java
@@ -0,0 +1,13 @@
+package com.iris.hackathon.txnvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxnValidatorPersisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxnValidatorPersisterApplication.class, args);
+	}
+
+}
